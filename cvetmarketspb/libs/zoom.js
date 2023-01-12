dd2601f3e429f1766dfc7d71e23d5fcef5995d49
maanukyaan <80559781+maanukyaan@users.noreply.github.com>
2023-01-13 00:00:49 +0300

    Add files via upload

---
+++ cvetmarketspb/libs/zoom.js
@@ -0,0 +1,13 @@
+if(window.innerWidth < 1000)
+{
+    mediumZoom('.zoomImg', {
+    margin: 50,
+    background: "rgba(46, 46, 41, 0.3)",
+});
+}
+else {
+    mediumZoom('.zoomImg', {
+    margin: 20,
+    background: "rgba(46, 46, 41, 0.8)",
+})
+}
